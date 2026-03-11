d66eebf52ecac9d8d9ab1d687e484301e625de93
ipbseries-scoreboard <ipbseries@gmail.com>
2026-03-11 19:48:11 +0100

    Delete test_peer.js

--- test_peer.js
+++
@@ -1,2 +0,0 @@
-const { Peer } = require('peerjs-on-node');
-# Wait, I don't have peerjs installed in Node. I will just run a python script that fetches the peerjs.min.js file and searches for default iceServers.
